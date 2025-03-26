0025e57c143e451368eb9fc4d7eaa1c9c52fa545
Warley Santos <136998025+warleysantosbr@users.noreply.github.com>
2025-03-26 17:27:39 -0300

    finish

--- 04 QrCode - 26-03-2025/script.js
+++ 04 QrCode - 26-03-2025/script.js
@@ -1,7 +1,7 @@
 const download = document.querySelector(".download");
 const dark = document.querySelector(".dark");
 const light = document.querySelector(".light");
-const qrContainer = document.querySelector("#qrCode");
+const qrContainer = document.querySelector("#qr-code");
 const qrText = document.querySelector(".qr-text");
 const shareBtn = document.querySelector(".share-btn");
 const sizes = document.querySelector(".sizes")
@@ -10,12 +10,12 @@ const sizes = document.querySelector(".sizes")
 dark.addEventListener("input", handleDarkColor);
 light.addEventListener("input", handleLightColor);
 qrText.addEventListener("input", handleQRText);
-sizes.addEventListener("input", handleSize);
-shareBtn.addEventListener("input", handleShare);
+sizes.addEventListener("change", handleSize);
+shareBtn.addEventListener("click", handleShare);
 
 const defaultUrl = "https://youtube.com/WarleySantosBr";
 let colorLight = "#fff", 
-    colorDark = "#00",
+    colorDark = "#000",
     text = defaultUrl,
     size = 300;
 
@@ -40,7 +40,7 @@ function handleQRText(e) {
 
 async function generateQRCode() {
     qrContainer.innerHTML = "";
-    new generateQRCode("qr-code", {
+    new QRCode("qr-code", {
         text,
         height: size,
         width: size,
@@ -50,4 +50,40 @@ async function generateQRCode() {
     download.href = await resolveDataUrl();
 }
 
-//https://youtu.be/I50Xwve6QW4?si=dXFuchN8Xs-Ua2dz&t=655
+async function handleShare() {
+    setTimeout(async () => {
+        try {
+            const base64url = await resolveDataUrl();
+            const blob = await (await fetch(base64url)).blob();
+            const file = new File([blob], "QRCode.png", {
+                type: blob.type,
+            });
+            await navigator.share({
+                files: [file],
+                title: text,
+            });
+        } catch (error) {
+            alert("Seu navegador não é compatível no momento.");
+        }
+    }, 100);
+}
+
+function handleSize(e) {
+    size = e.target.value;
+    generateQRCode();
+}
+
+function resolveDataUrl() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const img = document.querySelector("#qr-code img");
+            if(img.currentSrc){
+                resolve(img.currentSrc);
+                return;
+            }
+            const canvas = document.querySelector("canvas");
+            resolve(canvas.toDataURL());
+        }, 50);
+    });
+}
+generateQRCode();
